bcc95c796620317e4f0a15e71e3c8663755ec8fd
Asaduzzaman04 <asadchy152@gmail.com>
2024-12-01 17:15:05 +0600

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Camp from "@/components/Home/Camp";
 import Guide from "@/components/Home/Guide";
 import Feature from "@/components/Home/Feature";
 import GetApp from "@/components/Home/GetApp";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Guide />
       <Feature />
       <GetApp />
+      <Footer/>
     </>
   );
 }
